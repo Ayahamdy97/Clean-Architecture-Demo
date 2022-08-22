326a27110c229f3c2f80c8e86ce8c089781c3fe7
Aya hamdy <35396186+Ayahamdy97@users.noreply.github.com>
2022-08-23 01:06:09 +0200

    Initial Commit

---
+++ CleanArchitectureDemo/CleanArchitectureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureDemoApp.swift
+//  CleanArchitectureDemo
+//
+//  Created by Aya  on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
